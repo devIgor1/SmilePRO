23bac79962b9cc5f709267740c878467b861d484
devIgor1 <igormraesmm@gmail.com>
2025-10-31 20:22:43 -0300

    feat: integrate HomeHeader component into Home page layout for improved structure

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,3 +1,10 @@
+import { HomeHeader } from "./_components/HomeHeader";
+
 export default function Home() {
-  return <div className="text-3xl font-bold underline">Home Page</div>;
+  return (
+    <div className="flex min-h-screen flex-col w-full" >
+      <HomeHeader />
+      <div className="text-3xl font-bold underline">Home Page</div>
+    </div>
+  );
 }
