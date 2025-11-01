8cbd7e8ba02dc09da895f4c6a9f94b38ca5450e1
devIgor1 <igormraesmm@gmail.com>
2025-11-01 18:21:11 -0300

    feat: integrate Features component into Home page to highlight clinic functionalities

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,13 @@
 import { HomeHeader } from "./_components/HomeHeader";
 import { Hero } from "./_components/Hero";
+import { Features } from "./_components/Features";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col w-full" >
       <HomeHeader />
       <Hero />
+      <Features />
     </div>
   );
 }
