daf49b09a6c405d47519c62a8dc543dc899fb152
devIgor1 <igormraesmm@gmail.com>
2025-11-05 08:50:43 -0300

    feat: implement session validation in Dashboard component to redirect unauthenticated users

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,3 +1,12 @@
-export default function Dashboard() {
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return <div>Dashboard Page</div>;
 }
