2640642491e0b4e1542ac1d992ec803cef29bee7
devIgor1 <igormraesmm@gmail.com>
2025-12-04 23:47:47 -0300

    chore(next.config.ts): add output file tracing for Prisma client
    - Include output file tracing configuration for Prisma client in the Next.js setup, ensuring proper handling of API routes and client files during the build process.

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
 };
 
 export default nextConfig;
